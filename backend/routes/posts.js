ae02c79427ae0efd3651185acfe980a5aadfda84
siimlangel <siimlangel11@hotmail.com>
2020-12-07 13:32:04 +0200

    Adding posts endpoint

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,43 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authorize = require('../middlewares/authorize');
-const PostModel = require('../models/PostModel');
+const authorize = require("../middlewares/authorize");
+const PostModel = require("../models/PostModel");
 
-
-router.get('/', authorize, (request, response) => {
-
+router.get("/", authorize, (request, response) => {
     // Endpoint to get posts of people that currently logged in user follows or their own posts
 
     PostModel.getAllForUser(request.currentUser.id, (postIds) => {
-
         if (postIds.length) {
             PostModel.getByIds(postIds, request.currentUser.id, (posts) => {
-                response.status(201).json(posts)
+                response.status(201).json(posts);
             });
             return;
         }
-        response.json([])
-
-    })
-
+        response.json([]);
+    });
 });
 
-router.post('/', authorize,  (request, response) => {
-
-    // Endpoint to create a new post
-
+router.post("/", authorize, (request, response) => {
+    const post = request.body;
+    const userId = request.currentUser.id;
+    PostModel.create({ userId, ...post }, (err, rows) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log("Created new post");
+    });
 });
 
-
-router.put('/:postId/likes', authorize, (request, response) => {
-
+router.put("/:postId/likes", authorize, (request, response) => {
     // Endpoint for current user to like a post
 });
 
-router.delete('/:postId/likes', authorize, (request, response) => {
-
+router.delete("/:postId/likes", authorize, (request, response) => {
     // Endpoint for current user to unlike a post
-
 });
 
 module.exports = router;
