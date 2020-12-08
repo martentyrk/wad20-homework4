3964246c83a74a7c1108626c5d659be9cc6806a7
siimlangel <siimlangel11@hotmail.com>
2020-12-08 15:26:47 +0200

    Bonus task

--- backend/middlewares/authorize.js
+++ backend/middlewares/authorize.js
@@ -1,7 +1,7 @@
-const UserModel = require('../models/UserModel');
+const UserModel = require("../models/UserModel");
+const jwt = require("../library/jwt");
 
 module.exports = (request, response, next) => {
-
     // This is the place where you will need to implement authorization
     /*
         Pass access token in the Authorization header and verify
@@ -9,8 +9,17 @@ module.exports = (request, response, next) => {
         decoded user from access token.
     */
 
-    if (request.headers.authorization) {
-        UserModel.getById(1, (user) => {
+    // Get authorization from headers
+    const auth = request.headers.authorization;
+    if (auth) {
+        // Auth is in Bearer token format so split that
+        const token = auth.split(" ")[1];
+
+        // Decode jwt to get user id
+        const user = jwt.verifyAccessToken(token);
+
+        // Get user by id
+        UserModel.getById(user.id, (user) => {
             request.currentUser = user;
             next();
         });
@@ -18,7 +27,7 @@ module.exports = (request, response, next) => {
         // if there is no authorization header
 
         return response.status(403).json({
-            message: 'Invalid token'
+            message: "Invalid token",
         });
     }
 };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,30 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authorize = require('../middlewares/authorize');
-const UserModel = require('../models/UserModel');
-const FollowModel = require('../models/FollowModel');
+const authorize = require("../middlewares/authorize");
+const UserModel = require("../models/UserModel");
+const FollowModel = require("../models/FollowModel");
+const jwt = require("../library/jwt");
 
 // Public endpoints
-router.post('/', (request, response) => {
-
+router.post("/", (request, response) => {
     let form = {
-        email: {required: true},
-        password: {required: true},
-        firstname: {required: true},
-        lastname: {required: false},
-        avatar: {required: false}
+        email: { required: true },
+        password: { required: true },
+        firstname: { required: true },
+        lastname: { required: false },
+        avatar: { required: false },
     };
 
     const fieldMissing = {
         code: null,
-        message: 'Please provide %s field'
+        message: "Please provide %s field",
     };
 
     for (let field in form) {
         if (form[field].required === true && !request.body[field]) {
-
             fieldMissing.code = field;
-            fieldMissing.message = fieldMissing.message.replace('%s', field);
+            fieldMissing.message = fieldMissing.message.replace("%s", field);
 
             response.json(fieldMissing, 400);
             return;
@@ -34,9 +33,9 @@ router.post('/', (request, response) => {
     UserModel.getByEmail(request.body.email, (rows) => {
         if (Array.isArray(rows) && rows.length) {
             response.status(400).json({
-                code: 'email_already_exists',
-                message: 'User with such email already exists'
-            })
+                code: "email_already_exists",
+                message: "User with such email already exists",
+            });
         }
     });
 
@@ -45,27 +44,26 @@ router.post('/', (request, response) => {
         password: request.body.password,
         firstname: request.body.firstname,
         lastname: request.body.lastname,
-        avatar: request.body.avatar
+        avatar: request.body.avatar,
     };
 
     UserModel.create(params, () => {
-        response.status(201).json()
+        response.status(201).json();
     });
 });
 
-router.post('/login', (request, response) => {
-
+router.post("/login", (request, response) => {
     let email = request.body.email;
     let password = request.body.password;
 
     const invalidCredentials = {
-        code: 'invalid_credentials',
-        message: 'User with such credentials can not be found'
+        code: "invalid_credentials",
+        message: "User with such credentials can not be found",
     };
 
     const noCredentials = {
-        code: 'no_credentials',
-        message: 'Please provide email and password'
+        code: "no_credentials",
+        message: "Please provide email and password",
     };
 
     if (!email || !password) {
@@ -79,91 +77,85 @@ router.post('/login', (request, response) => {
             return;
         }
 
+        // Create token with user id to retrieve from database later
+        const token = jwt.createAccessToken({ id: user.id });
+
         response.json({
             id: user.id,
             firstname: user.firstname,
             lastname: user.lastname,
             email: user.email,
             avatar: user.avatar,
-            accessToken: null // THis is the place where you should pass generated access token
-        })
+            accessToken: token, // THis is the place where you should pass generated access token
+        });
     });
 });
 
 // Protected endpoints
-router.get('/', authorize, (request, response) => {
-
+router.get("/", authorize, (request, response) => {
     UserModel.getAll(request.currentUser.id, (users) => {
-        response.status(200).json(users)
+        response.status(200).json(users);
     });
 });
 
-router.post('/:userId/follows', authorize, (request, response) => {
-
+router.post("/:userId/follows", authorize, (request, response) => {
     //To get currently logged in user object use request.currentUser
 
     let userId = request.params.userId;
     let followerId = request.currentUser.id;
 
     if (userId.toString() === followerId.toString()) {
-        response.status(403)
-            .json({
-                code: 'can_not_follow_yourself',
-                message: 'You can not follow yourself'
-            });
+        response.status(403).json({
+            code: "can_not_follow_yourself",
+            message: "You can not follow yourself",
+        });
         return;
     }
 
     FollowModel.getByUserIdAndFollowerId(userId, followerId, (rows) => {
-
         if (rows.length) {
-            response.status(409)
-                .json({
-                    code: 'already_following',
-                    message: 'You are already following this user'
-                });
+            response.status(409).json({
+                code: "already_following",
+                message: "You are already following this user",
+            });
         } else {
             FollowModel.create(userId, followerId, () => {
                 response.json({
-                    ok: true
-                })
-            })
+                    ok: true,
+                });
+            });
         }
-    })
+    });
 });
 
-router.delete('/:userId/follows', authorize, (request, response) => {
-
+router.delete("/:userId/follows", authorize, (request, response) => {
     //To get currently logged in user object use request.currentUser
 
     let userId = request.params.userId;
     let followerId = request.currentUser.id;
 
     if (userId.toString() === followerId.toString()) {
-        response.status(403)
-            .json({
-                code: 'can_not_unfollow_yourself',
-                message: 'You can not unfollow yourself'
-            });
+        response.status(403).json({
+            code: "can_not_unfollow_yourself",
+            message: "You can not unfollow yourself",
+        });
         return;
     }
 
     FollowModel.getByUserIdAndFollowerId(userId, followerId, (rows) => {
-
         if (!rows.length) {
-            response.status(409)
-                .json({
-                    code: 'not_following',
-                    message: 'You are not following this user'
-                });
+            response.status(409).json({
+                code: "not_following",
+                message: "You are not following this user",
+            });
         } else {
             FollowModel.delete(userId, followerId, () => {
                 response.json({
-                    ok: true
-                })
-            })
+                    ok: true,
+                });
+            });
         }
-    })
+    });
 });
 
 module.exports = router;
